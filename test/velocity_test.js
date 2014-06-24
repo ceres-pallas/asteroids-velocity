ed2b496766e92b648854f8315fdf5a605e7ae48c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-24 16:36:00 +0200

    correct indent of velocity_test.js

--- test/velocity_test.js
+++ test/velocity_test.js
@@ -4,152 +4,152 @@ var Velocity = require('../lib/velocity');
 
 describe('Velocity', function(){
     it('should exist', function(){
-	expect(Velocity).to.exist;
+        expect(Velocity).to.exist;
     });
 
     it('should be a constructor', function(){
-	expect(Velocity).to.be.a('function');
+        expect(Velocity).to.be.a('function');
     });
 
     it('should be an instance of BaseObject', function(){
-	expect(new Velocity()).to.be.an.instanceof(require('asteroids-object'));
+        expect(new Velocity()).to.be.an.instanceof(require('asteroids-object'));
     });
 
     describe('object', function(){
-	var velocity;
+        var velocity;
 
-	beforeEach(function(){
-	    velocity = new Velocity();
-	});
+        beforeEach(function(){
+            velocity = new Velocity();
+        });
 
-	['speed', 'heading', 'velocity', 'omega', 'tick', 'state'].forEach(function(methodName){
-	    it('should respond to ' + methodName, function(){
-		expect(velocity).to.respondTo(methodName);
-	    });
-	});
+        ['speed', 'heading', 'velocity', 'omega', 'tick', 'state'].forEach(function(methodName){
+            it('should respond to ' + methodName, function(){
+                expect(velocity).to.respondTo(methodName);
+            });
+        });
     });
 
     describe('interactions', function(){
-	var velocity;
+        var velocity;
 
-	beforeEach(function(){
-	    velocity = new Velocity;
-	});
+        beforeEach(function(){
+            velocity = new Velocity;
+        });
 
-	it('should have a default speed', function(){
-	    expect(velocity.speed()).to.equal(0);
-	});
+        it('should have a default speed', function(){
+            expect(velocity.speed()).to.equal(0);
+        });
 
-	it('should be able to set the speed', function(){
-	    var value = 1;
+        it('should be able to set the speed', function(){
+            var value = 1;
 
-	    velocity.speed(value);
+            velocity.speed(value);
 
-	    expect(velocity.speed()).to.equal(value);
-	});
+            expect(velocity.speed()).to.equal(value);
+        });
 
-	it('should have a default heading', function(){
-	    expect(velocity.heading()).to.equal(0);
-	});
+        it('should have a default heading', function(){
+            expect(velocity.heading()).to.equal(0);
+        });
 
-	it('should be able to set the heading', function(){
-	    var value = Math.PI/3;
+        it('should be able to set the heading', function(){
+            var value = Math.PI/3;
 
-	    velocity.heading(value);
+            velocity.heading(value);
 
-	    expect(velocity.heading()).to.equal(value);
-	});
+            expect(velocity.heading()).to.equal(value);
+        });
 
-	it('should have a default velocity', function(){
-	    expect(velocity.velocity()).to.deep.equal({ speed: 0, heading: 0, omega: 0 });
-	});
+        it('should have a default velocity', function(){
+            expect(velocity.velocity()).to.deep.equal({ speed: 0, heading: 0, omega: 0 });
+        });
 
-	it('should be able to set the velocity', function(){
-	    var value = {
-		'speed': 1,
-		'heading': Math.PI/3,
-		'omega': 0
-	    };
+        it('should be able to set the velocity', function(){
+            var value = {
+                'speed': 1,
+                'heading': Math.PI/3,
+                'omega': 0
+            };
 
-	    velocity.velocity(value);
+            velocity.velocity(value);
 
-	    expect(velocity.velocity()).to.deep.equal(value);
-	});
+            expect(velocity.velocity()).to.deep.equal(value);
+        });
 
-	it('should have a default omega', function(){
-	    expect(velocity.omega()).to.equal(0);
-	});
+        it('should have a default omega', function(){
+            expect(velocity.omega()).to.equal(0);
+        });
 
-	it('should be able to set the omega', function(){
-	    var value = Math.PI/6;
+        it('should be able to set the omega', function(){
+            var value = Math.PI/6;
 
-	    velocity.omega(value);
+            velocity.omega(value);
 
-	    expect(velocity.omega()).to.equal(value);
-	});
+            expect(velocity.omega()).to.equal(value);
+        });
 
-	[
-	    function(velocity){ velocity.speed(1.0); },
-	    function(velocity){ velocity.heading(Math.PI); },
-	    function(velocity){ velocity.omega(Math.PI/2); },
-	    function(velocity){ velocity.velocity({ 'speed': 1.0 }); },
-	    function(velocity){ velocity.velocity({ 'heading': Math.PI }); },
-	    function(velocity){ velocity.velocity({ 'omega': Math.PI/2 }); },
-	    function(velocity){ velocity.velocity({ 'speed': 1.0, 'heading': Math.PI }); },
-	    function(velocity){ velocity.velocity({ 'speed': 1.0, 'omega': Math.PI/2 }); },
-	    function(velocity){ velocity.velocity({ 'heading': Math.PI, 'omega': Math.PI/2 }); },
-	    function(velocity){ velocity.velocity({ 'speed': 1.0, 'heading': Math.PI, 'omega': Math.PI/2 }); },
-	].forEach(function(mutator){
-	    it('should notify upon changes', function(done){
-		velocity.addListener('velocity', function(){ done() });
+        [
+            function(velocity){ velocity.speed(1.0); },
+            function(velocity){ velocity.heading(Math.PI); },
+            function(velocity){ velocity.omega(Math.PI/2); },
+            function(velocity){ velocity.velocity({ 'speed': 1.0 }); },
+            function(velocity){ velocity.velocity({ 'heading': Math.PI }); },
+            function(velocity){ velocity.velocity({ 'omega': Math.PI/2 }); },
+            function(velocity){ velocity.velocity({ 'speed': 1.0, 'heading': Math.PI }); },
+            function(velocity){ velocity.velocity({ 'speed': 1.0, 'omega': Math.PI/2 }); },
+            function(velocity){ velocity.velocity({ 'heading': Math.PI, 'omega': Math.PI/2 }); },
+            function(velocity){ velocity.velocity({ 'speed': 1.0, 'heading': Math.PI, 'omega': Math.PI/2 }); },
+        ].forEach(function(mutator){
+            it('should notify upon changes', function(done){
+                velocity.addListener('velocity', function(){ done() });
 
-		mutator(velocity);
-	    });
-	});
+                mutator(velocity);
+            });
+        });
     });
 
     describe('tick', function(){
-	var velocity;
+        var velocity;
 
-	beforeEach(function(){
-	    velocity = new Velocity();
-	    velocity.position({ x: 0, y: 0 });
-	    velocity.velocity({ speed: 1, heading: 0 });
-	});
+        beforeEach(function(){
+            velocity = new Velocity();
+            velocity.position({ x: 0, y: 0 });
+            velocity.velocity({ speed: 1, heading: 0 });
+        });
 
-	[
-	    { heading: 0,          x:  1.0, y:  0.0 },
-	    { heading: Math.PI,    x: -1.0, y:  0.0 },
-	    { heading: Math.PI/2,  x:  0.0, y:  1.0 },
-	    { heading: -Math.PI/2, x:  0.0, y: -1.0 },
-	].forEach(function(testCase){
-	    it('should update position when heading is ' + testCase.heading, function(){
-		velocity.velocity(testCase);
+        [
+            { heading: 0,          x:  1.0, y:  0.0 },
+            { heading: Math.PI,    x: -1.0, y:  0.0 },
+            { heading: Math.PI/2,  x:  0.0, y:  1.0 },
+            { heading: -Math.PI/2, x:  0.0, y: -1.0 },
+        ].forEach(function(testCase){
+            it('should update position when heading is ' + testCase.heading, function(){
+                velocity.velocity(testCase);
 
-		velocity.tick();
+                velocity.tick();
 
-		expect(velocity.x()).to.be.closeTo(testCase.x, 0.0001);
-	    });
+                expect(velocity.x()).to.be.closeTo(testCase.x, 0.0001);
+            });
 
-	});
+        });
     });
 
     describe('state', function(){
-	var velocity;
+        var velocity;
 
-	beforeEach(function(){
-	    velocity = new Velocity();
-	    velocity.position({ x: 0, y: 0 });
-	    velocity.velocity({ speed: 1, heading: 0 });
-	});
+        beforeEach(function(){
+            velocity = new Velocity();
+            velocity.position({ x: 0, y: 0 });
+            velocity.velocity({ speed: 1, heading: 0 });
+        });
 
-	['x', 'y', 'radius', 'orientation', 'speed', 'heading', 'omega'].forEach(function(key){
-	    it('should contain the key ' + key, function(){
-		var state = velocity.state();
+        ['x', 'y', 'radius', 'orientation', 'speed', 'heading', 'omega'].forEach(function(key){
+            it('should contain the key ' + key, function(){
+                var state = velocity.state();
 
-		expect(state).to.contain.keys(key);
-	    });
-	});
+                expect(state).to.contain.keys(key);
+            });
+        });
     });
 
 });
